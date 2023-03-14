e4e0d0b788d459d6ee7b432306a24969706cb2c8
Edgar Silva <edgarasdaa10@gmail.com>
2023-03-14 12:04:07 +0000

    Fix typography.

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -5,8 +5,8 @@ import { remark } from "remark";
 import html from "remark-html";
 
 const getPage = async () => {
-  console.log("Getting page");
   const mdblocks = await n2m.pageToMarkdown("51e421206a914d299def6b106a1e1b03");
+
   const markdown = n2m.toMarkdownString(mdblocks);
 
   return String(await remark().use(html).process(markdown));
@@ -15,10 +15,10 @@ const getPage = async () => {
 const Blog = async () => {
   const page = await getPage();
   return (
-    <>
+    <article className="prose mx-auto">
       <h1>Test Blog</h1>
       <div dangerouslySetInnerHTML={{ __html: page }} />
-    </>
+    </article>
   );
 };
 
