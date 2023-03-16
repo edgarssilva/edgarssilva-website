9ddbee30dcd586167c7e97ae3b6d460a3d6869b4
Edgar Silva <edgarasdaa10@gmail.com>
2023-03-16 18:27:25 +0000

    Header links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,8 +14,19 @@ const Page = () => {
         A Master&apos;s student in Software Engineering. Welcome to my personal
         place.
         <br />
-        Here you can look through my blog, learn a little more about me and my
-        projects.
+        Here you can look through{" "}
+        <a className="text-blue-600 dark:text-blue-400" href="/blog">
+          my blog
+        </a>
+        , learn a little more{" "}
+        <a className="text-blue-600 dark:text-blue-400" href="about-me">
+          about me
+        </a>{" "}
+        and{" "}
+        <a className="text-blue-600 dark:text-blue-400" href="/portfolio">
+          my projects
+        </a>
+        .
       </h3>
     </header>
   );
